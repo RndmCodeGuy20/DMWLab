5d07f6d0c8bccbc8841ebda972b7a90f1cd1dc7a
maneshantanu20 <manesg@rknec.edu>
2023-04-01 20:26:54 +0530

    chore: PRACTICAL 1 (WIP) (ABANDONED) : master

--- Practical 0/Class Queries.sql
+++ Practical 0/Class Queries.sql
@@ -1,49 +1,49 @@
-# GROUP-BY QUERIES ON :-
+-- GROUP-BY QUERIES ON :-
 
-# Continent
-SELECT location_id, continent, SUM(dollars_sold) "TOTAL SALES"
-FROM locations_table
-GROUP BY continent;
+-- Continent
+SELECT L.LOC_ID, L.COUNTRY, SUM(UNITS_SOLD) "TOTAL SALES"
+FROM SALES S LEFT JOIN LOCATIONS L on L.LOC_ID = S.LOC_ID
+GROUP BY L.COUNTRY, L.LOC_ID;
 
-# Country
+-- Country
 SELECT location_id, country, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY country;
 
-# State
+-- State
 SELECT location_id, state, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY state;
 
-# City
+-- City
 SELECT location_id, city, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY city;
 
 
-# Grouping Sets (group by multiple columns) :-
+-- Grouping Sets (group by multiple columns) :-
 
-# Continent, Country
+-- Continent, Country
 SELECT location_id, continent, country, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY continent, country
 WITH ROLLUP;
 
 
-# Country, State
+-- Country, State
 SELECT location_id, country, state, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY country, state
 WITH ROLLUP;
 
-# State, City
+-- State, City
 SELECT location_id, state, city, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY state, city
 WITH ROLLUP;
 
 
-# Continent WITH ROLLUP
+-- Continent WITH ROLLUP
 SELECT location_id, continent, SUM(dollars_sold) "TOTAL SALES"
 FROM locations_table
 GROUP BY continent
